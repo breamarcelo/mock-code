6ccca0ba66cd447e649f8b73b2997f0ac263b3fc
breamarcelo <brea.marcelo@gmail.com>
2025-09-07 03:41:37 +0200

    started TetrisConsole

---
+++ CSharp-TetrisConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace CSharp_TetrisConsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[,] pieceA = { { "[*]", "[ ]", "[ ]" }, { "[*]", "[*]", "[*]" } };
+
+        for (int i = 0; i < pieceA.GetLength(0); i++)
+        {
+            for (int j = 0; j < pieceA.GetLength(1); j++)
+            {
+                Console.Write(pieceA[i, j]);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("----ROTATED----");
+        string[,] pieceB = { { "[*]", "[*]" }, { "[*]", "[ ]" }, { "[*]", "[ ]" } };
+
+        for (int i = 0; i < pieceB.GetLength(0); i++)
+        {
+            for (int j = 0; j < pieceB.GetLength(1); j++)
+            {
+                Console.Write(pieceB[i, j]);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("----ROTATED----");
+        string[,] pieceC = { { "[*]", "[*]", "[*]" }, { "[ ]", "[ ]", "[*]" } };
+
+        for (int i = 0; i < pieceC.GetLength(0); i++)
+        {
+            for (int j = 0; j < pieceC.GetLength(1); j++)
+            {
+                Console.Write(pieceC[i, j]);
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("----ROTATED----");
+        string[,] pieceD = { { "[ ]", "[*]" }, { "[ ]", "[*]" }, { "[*]", "[*]" } };
+
+        for (int i = 0; i < pieceD.GetLength(0); i++)
+        {
+            for (int j = 0; j < pieceD.GetLength(1); j++)
+            {
+                Console.Write(pieceD[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
