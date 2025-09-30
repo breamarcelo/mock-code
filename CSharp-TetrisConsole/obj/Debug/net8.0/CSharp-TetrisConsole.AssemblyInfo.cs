780b838902109a54199f19a4b6017cd294e1ffb2
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-01 00:00:43 +0200

    updates

--- CSharp-TetrisConsole/obj/Debug/net8.0/CSharp-TetrisConsole.AssemblyInfo.cs
+++ CSharp-TetrisConsole/obj/Debug/net8.0/CSharp-TetrisConsole.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp-TetrisConsole")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6333636da721c91fc15c738eca5d1880f39fbd7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0861a951b4996cdf5263b22a24e7cbb0e191a51b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp-TetrisConsole")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp-TetrisConsole")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
