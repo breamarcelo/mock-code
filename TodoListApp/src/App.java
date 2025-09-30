65acbecd1e5cb96770a175f75e4a9f6f55f56aca
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-01 00:27:13 +0200

    started TodoListApp

--- TodoListApp/src/App.java
+++ TodoListApp/src/App.java
@@ -1,5 +1,19 @@
+
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Item> itemsList = new ArrayList< Item>(){
+            {
+                add(new Item("Buy", "All groceries for the party"));
+                add(new Item("Call wife", "Call to remind her to feed the cat"));
+                add(new Item("Mechanic", "Take the car to the mechanic to get a checkup"));
+                add(new Item("Emil Mike", "Send Mike an evite to the house BBQ"));
+                add(new Item("Pickup mom", "Remember to pickup mom from the airpot"));
+            }
+        };
+        for(int i=0; i<itemsList.size(); i++){
+            System.out.println((i+1) + ". " + itemsList.get(i));
+        }
     }
 }
---
+++ TodoListApp/src/Item.java
@@ -0,0 +1,34 @@
+
+
+public class Item {
+    private String title;
+    private String description;
+
+    public Item(String title, String description) {
+        this.description = description;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return this.getTitle() + "\n" + this.getDescription();
+    }
+    
+    
+}
