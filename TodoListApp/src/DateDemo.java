d73e4284bb8fda123d4c07fc451ac18c67e45648
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-04 22:36:15 +0200

    working with dates and calendar

---
+++ TodoListApp/src/DateDemo.java
@@ -0,0 +1,11 @@
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String[] args) {
+        Date date = new Date();
+        Calendar cal = Calendar.getInstance();
+        System.out.println(date);
+        System.out.println(cal);
+    }
+}
