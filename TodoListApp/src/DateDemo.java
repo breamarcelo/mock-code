4f5c1310dc4fade997584be3eeaa567ac89abf07
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-05 23:58:50 +0200

    added formatting

--- TodoListApp/src/DateDemo.java
+++ TodoListApp/src/DateDemo.java
@@ -1,4 +1,5 @@
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -8,7 +9,10 @@ public class DateDemo {
         Calendar cal = Calendar.getInstance();
         System.out.println(date);
         //System.out.println(cal);
-        LocalDate ld = LocalDate.now();
+        LocalDate ld = LocalDate.of(2025, 10, 3);
+        DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("dd-mm-yyyy");
+        System.out.println("Before formatting:");
         System.out.println(ld);
+        System.out.println("After formatting:");
     }
 }
