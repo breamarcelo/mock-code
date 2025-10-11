04955f460d29ded059d4b5f4b768bc5a9400bed6
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-11 18:43:28 +0200

    created dates list and sorted from oldest to recent

--- TodoListApp/src/DateDemo.java
+++ TodoListApp/src/DateDemo.java
@@ -1,20 +1,36 @@
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class DateDemo {
     public static void main(String[] args) {
-        Date date = new Date();
-        Calendar cal = Calendar.getInstance();
-        System.out.println(date);
-        //System.out.println(cal);
-        LocalDate ld = LocalDate.of(2025, 10, 3);
+        ArrayList<LocalDate> datesList = new ArrayList<LocalDate>();
+
+        LocalDate date1 = LocalDate.of(2025, 10, 3);
+        LocalDate date2 = LocalDate.of(1992, 7, 21);
+        LocalDate date3 = LocalDate.of(1999, 6, 14);
+        LocalDate date4 = LocalDate.of(2019, 4, 18);
+        LocalDate date5 = LocalDate.of(1987, 1, 16);
+
+        datesList.add(date1);
+        datesList.add(date2);
+        datesList.add(date3);
+        datesList.add(date4);
+        datesList.add(date5);
+
         DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        System.out.println("Before formatting:");
-        System.out.println(ld);
-        System.out.println("After formatting:");
-        System.out.println(ld.format(myFormat));
-        // added comment
+        System.out.println("before sort:");
+        for(LocalDate date : datesList){
+            System.out.println(date.format(myFormat));
+        }
+        Collections.sort(datesList);
+        System.out.println("after sort:");
+        for(LocalDate date : datesList){
+            System.out.println(date.format(myFormat));
+        }
+        System.out.println("Selected element [1]");
+        System.out.println(datesList.get(1).format(myFormat));
+
     }
 }
