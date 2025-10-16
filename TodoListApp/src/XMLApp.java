acc1af8fc9d3ac6ac6c3f777383f150435dce12d
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-16 15:25:52 +0200

    created update method

--- TodoListApp/src/XMLApp.java
+++ TodoListApp/src/XMLApp.java
@@ -15,8 +15,8 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class XMLApp {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws ParserConfigurationException, TransformerException {
-        Scanner sc = new Scanner(System.in);
         DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
         Document doc = null;
         
@@ -43,7 +43,7 @@ public class XMLApp {
          String apellidos = sc.nextLine();
          System.out.println("Departamento: ");
          String departamento = sc.nextLine();
-         System.out.println("Código epartamento: ");
+         System.out.println("Código departamento: ");
          int codDept = sc.nextInt();
          System.out.println("Salario: ");
          double salario = sc.nextDouble();
@@ -52,6 +52,8 @@ public class XMLApp {
          addEmpleado(doc, id, nombre, apellidos, departamento, codDept, salario);
          */
 
+         showEmpleados(doc);
+         actualizarEmpleado(doc);
          showEmpleados(doc);
         
     }
@@ -89,9 +91,70 @@ public class XMLApp {
     public static void showEmpleados(Document doc){
         NodeList list = doc.getElementsByTagName("empleado");
         for(int i=0; i < list.getLength(); i++){
-            String id = list.item(i).getAttributes().getNamedItem("ID").getTextContent();
+            Element n = (Element) list.item(i);
+            String id = n.getAttributes().getNamedItem("ID").getTextContent();
+            String nombre = n.getElementsByTagName("nombre").item(0).getTextContent();
+            String apellidos = n.getElementsByTagName("apellidos").item(0).getTextContent();
+            String codDept = n.getElementsByTagName("departamento").item(0).getAttributes().getNamedItem("codDept").getTextContent();
+            String departamento = n.getElementsByTagName("departamento").item(0).getTextContent();
+            String salario = n.getElementsByTagName("salario").item(0).getTextContent();
             System.out.println("ID: " + id);
-            
+            System.out.println("Nombre: " + nombre + " " + apellidos);
+            System.out.println("Departamento: " + codDept + ": " + departamento);
+            System.out.println("Salario: " + salario);
+        }
+    }
+
+    public static void actualizarEmpleado(Document doc){
+        System.out.println("Seleccione el empleado por ID:");
+        String id = Integer.toString(sc.nextInt());
+        sc.nextLine();
+        NodeList list = doc.getElementsByTagName("empleado");
+        for(int i=0; i<list.getLength(); i++){
+            Element n = (Element) list.item(i);
+            String empId = n.getAttributes().getNamedItem("ID").getTextContent();
+            if(empId.equals(id)){
+                System.out.println("Actualizar ID (" + empId +"):");
+                String inputId = Integer.toString(sc.nextInt());
+                sc.nextLine();
+                String empNombre = n.getElementsByTagName("nombre").item(0).getTextContent();
+                System.out.println("Actualizar nombre (" + empNombre +"):");
+                String inputNombre = sc.nextLine();
+                String empApellido = n.getElementsByTagName("apellidos").item(0).getTextContent();
+                System.out.println("Actualizar apellido (" + empApellido +"):");
+                String inputApellido = sc.nextLine();
+                String empCodDept = n.getElementsByTagName("departamento").item(0).getAttributes().getNamedItem("codDept").getTextContent();
+                System.out.println("Actualizar código departamento (" + empCodDept +"):");
+                String inputCodDept = Integer.toString(sc.nextInt());
+                sc.nextLine();
+                String empDepartamento = n.getElementsByTagName("departamento").item(0).getTextContent();
+                System.out.println("Actualizar departamento (" + empDepartamento +"):");
+                String inputDepartamento = sc.nextLine();
+                String empSalario = n.getElementsByTagName("salario").item(0).getTextContent();
+                System.out.println("Actualizar salario (" + empSalario +"):");
+                String inputSalario = Double.toString(sc.nextDouble());
+                sc.nextLine();
+
+                Element emp = doc.createElement("empleado");
+                doc.getDocumentElement().appendChild(emp);
+                emp.setAttribute("ID", inputId);
+                emp.appendChild(doc.createElement("nombre")).appendChild(doc.createTextNode(inputNombre));
+                emp.appendChild(doc.createElement("apellidos")).appendChild(doc.createTextNode(inputApellido));
+                Element dept = doc.createElement("departamento");
+                dept.appendChild(doc.createTextNode(inputDepartamento));
+                dept.setAttribute("codDept", inputCodDept);
+                emp.appendChild(dept);
+                emp.appendChild(doc.createElement("salario")).appendChild(doc.createTextNode(inputSalario));
+
+                doc.getDocumentElement().replaceChild(emp, n);
+
+                try {
+                    outputTransformer(doc);
+                } catch (TransformerException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
