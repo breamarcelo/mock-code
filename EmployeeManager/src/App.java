264cb44ea99ceb2ebaa6efcf44e9477ee45acdb4
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-10-22 10:31:16 +0200

    started resXmlFile method

--- EmployeeManager/src/App.java
+++ EmployeeManager/src/App.java
@@ -24,7 +24,7 @@ public class App {
 
         try {
             if(!f.exists()){
-                createXMLDocument(builder);
+                (builder);
             } 
             doc = builder.parse(f);
         } catch (SAXException | IOException e) {
--- javafxdemo/src/main/java/com/example/EmployeeParser.java
+++ javafxdemo/src/main/java/com/example/EmployeeParser.java
@@ -1,14 +1,61 @@
 package com.example;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class EmployeeParser {
+    
+    private File file = new File("Employees.xml");
+    private DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
 
-    public EmployeeParser() throws ParserConfigurationException {
-        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+    public EmployeeParser() throws ParserConfigurationException, TransformerConfigurationException, TransformerException, TransformerFactoryConfigurationError {
+        
+        if(!this.getFile().exists()){
+            Document doc = this.getBuilder().getDOMImplementation().createDocument(null, "Employees", null);
+            doc.setXmlVersion("1.0");
+            outputTransformer(doc);
+        }
+    }
+
+    public void outputTransformer(Document doc) throws TransformerConfigurationException, TransformerException, TransformerFactoryConfigurationError{
+        TransformerFactory.newInstance().newTransformer().transform(new DOMSource(doc), new StreamResult(this.getFile()));
+    }
+
+    public void readXmlFile() throws SAXException, IOException{
+        Document doc = this.getBuilder().parse(this.getFile());
+        NodeList list = doc.getElementsByTagName("employee");
+        for (int i = 0; i < list.getLength(); i++) {
+            Element emp = (Element) list.item(i);
+            String id = emp.getElementsByTagName("id").item(0).getTextContent();
+            String name = emp.getElementsByTagName("name").item(0).getTextContent();
+            String lName = emp.getElementsByTagName("lastName").item(0).getTextContent();
+            String deptIt = emp.getElementsByTagName("deptId").item(0).getTextContent();
+            String deptName = emp.getElementsByTagName("deptName").item(0).getTextContent();
+            String salary = emp.getElementsByTagName("salary").item(0).getTextContent();
+        }
+    }
+
+    public File getFile() {
+        return file;
+    }
+
+    public DocumentBuilder getBuilder() {
+        return builder;
     }
     
 }
