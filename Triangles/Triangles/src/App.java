2abb57b8d6bd4768c5424e65b3ba2433efe71aad
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-11-01 16:24:46 +0100

    started Triangles

---
+++ Triangles/Triangles/src/App.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Enter number of rows:");
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for(int i=1; i <= n; i++){
+            
+        }
+    }
+}
