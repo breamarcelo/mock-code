5ac3510f477c1932688377df5e41fee12150cae2
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-11-02 20:26:31 +0100

    adjust and center to screen size

--- JavaSwingDemos/FirstProject/src/MyFrame.java
+++ JavaSwingDemos/FirstProject/src/MyFrame.java
@@ -1,9 +1,13 @@
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame{
     public MyFrame() {
+        double screenW = Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        double screenH = Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+
         setVisible(true);
-        setSize(600, 450);
+        setBounds((int) screenW/4, (int) screenH/4, (int) screenW/2, (int) screenH/2);
         add(new MyPanel());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
