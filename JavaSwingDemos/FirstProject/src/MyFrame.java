e16076db0f1f311392113a76994394084b93aea3
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-11-02 18:48:13 +0100

    Started JAvaSwing FirstProject

---
+++ JavaSwingDemos/FirstProject/src/MyFrame.java
@@ -0,0 +1,11 @@
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+    public MyFrame() {
+        setVisible(true);
+        setSize(600, 450);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
