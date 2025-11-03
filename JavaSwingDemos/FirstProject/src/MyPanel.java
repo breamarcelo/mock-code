d195f5bb5c3150e32209744ace209db0a947054e
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2025-11-03 22:02:21 +0100

    added Button and Label, needs fixes

--- JavaSwingDemos/FirstProject/src/MyPanel.java
+++ JavaSwingDemos/FirstProject/src/MyPanel.java
@@ -1,9 +1,21 @@
+import java.awt.Button;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Label;
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
+    public MyPanel() {
+        Button btn = new Button();
+        btn.setSize(100, 100);
+        btn.setLabel("hello");
+        add(btn);
 
+        Label label1 = new Label("Hello World!");
+        label1.setBounds(350, 100, 100, 100);
+        add(label1);
+    }
+        
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -11,7 +23,5 @@ public class MyPanel extends JPanel {
         g.drawLine(50, 50, 150, 110);
         g.setColor(Color.darkGray);
         g.fillRect(200, 50, 100, 60);
-
-   
     }
 }
