0e483d82ccb626d1da30a0f7d01af18d6b4098d9
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-02-16 12:52:53 +0100

    JDBC conecction tested: OK!

--- hybernateJPADemo/demo/src/main/java/com/hybernatejpademo/Main.java
+++ hybernateJPADemo/demo/src/main/java/com/hybernatejpademo/Main.java
@@ -1,7 +1,38 @@
 package com.hybernatejpademo;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            MysqlDataSource ds = new MysqlDataSource();
+            ds.setUrl("jdbc:mysql://192.168.1.20/repasosql");
+            ds.setUser("marcelo");
+            ds.setPassword("R2daH$R4urL");
+            
+            Connection con = ds.getConnection();
+            ResultSet res = con.createStatement().executeQuery("SELECT * FROM TPersona");
+
+            while(res.next()){
+                System.out.println(
+                    "ID:" + " " + res.getString("pkID") + " \n" + 
+                    "Nombre:" + " " + res.getString("Nombre") + " \n" +
+                    "Apellidos:" + " " + res.getString("Apellidos") + " \n" +
+                    "Teléfono:" + " " + res.getString("Telefono") + " \n" +
+                    "Fecha de Nacimiento:" + " " + res.getString("FechaNac") + " \n" +
+                    "-----------------------------------------------------"
+                );
+            }
+
+            System.out.println("Connected!");
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+        // Persistence.generateSchema("finale", null);
+        // var emf = Persistence.createEntityManagerFactory("finale");
+        // var em = emf.createEntityManager();
     }
 }
