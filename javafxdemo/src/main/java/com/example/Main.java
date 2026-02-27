dc0d9ae5c30f89ced322a8c4cd5e0f90b7aebf33
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-02-27 23:34:00 +0100

    aded comments

--- javafxdemo/src/main/java/com/example/Main.java
+++ javafxdemo/src/main/java/com/example/Main.java
@@ -2,6 +2,8 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
+        // call JavaFX App
+        // neede for .jar file
         App.main(args);
     }
 }
