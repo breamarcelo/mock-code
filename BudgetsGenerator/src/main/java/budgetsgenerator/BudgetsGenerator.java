afc36f6fec1bd7cc39379acc3699684ba170ab54
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-02-28 07:45:39 +0100

    created BudgetsGenerator

---
+++ BudgetsGenerator/src/main/java/budgetsgenerator/BudgetsGenerator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package budgetsgenerator;
+
+/**
+ *
+ * @author marcelobrea
+ */
+public class BudgetsGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
