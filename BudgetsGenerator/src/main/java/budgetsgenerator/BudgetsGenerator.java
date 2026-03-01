bb31e9561622f3fbcd853bc56380debb8b1e0534
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-03-01 22:15:17 +0100

    failed launch

--- BudgetsGenerator/src/main/java/budgetsgenerator/BudgetsGenerator.java
+++ BudgetsGenerator/src/main/java/budgetsgenerator/BudgetsGenerator.java
@@ -4,13 +4,30 @@
 
 package budgetsgenerator;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author marcelobrea
  */
-public class BudgetsGenerator {
+public class BudgetsGenerator extends Application{
+    
+    @Override
+    public void start(Stage arg0) throws Exception {
+        Stage window = new Stage();
+        BorderPane root = new BorderPane();
+        root.setCenter(new GridPane());
+        Scene scene = new Scene(root);
+        window.setScene(scene);
+        window.show();
+
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch(args);
     }
 }
