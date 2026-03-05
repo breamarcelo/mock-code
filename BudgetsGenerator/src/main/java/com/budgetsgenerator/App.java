56f52ee96ec19cf01b3aa0ca00ac68066fa64ace
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-03-05 23:39:19 +0100

    Scene changing demo

--- BudgetsGenerator/src/main/java/com/budgetsgenerator/App.java
+++ BudgetsGenerator/src/main/java/com/budgetsgenerator/App.java
@@ -3,13 +3,10 @@ package com.budgetsgenerator;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -17,19 +14,28 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
-    private static Scene scene;
+    private static Scene scene1, scene2;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        Stage window = stage;
-        window.resizableProperty().set(false);
-        BorderPane pane = new BorderPane();
-        pane.setBackground(new Background(new BackgroundFill(Color.valueOf("#146732"), new CornerRadii(0), new Insets(0))));
-        pane.setMaxWidth(50);
-        Scene scene = new Scene(pane);
-        window.setScene(scene);
-        window.show();
-        window.centerOnScreen();
+    public void start(Stage primaryStage) throws IOException {
+        Label label1 = new Label("This is the first scene");
+        Button button1 = new Button("Go to Scene 2");
+        button1.setOnAction(e -> primaryStage.setScene(scene2));
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(label1, button1);
+        scene1 = new Scene(layout1, 300, 250);
+
+        // Scene 2
+        Label label2 = new Label("This is the second scene");
+        Button button2 = new Button("Go to Scene 1");
+        button2.setOnAction(e -> primaryStage.setScene(scene1));
+        VBox layout2 = new VBox(20);
+        layout2.getChildren().addAll(label2, button2);
+        scene2 = new Scene(layout2, 300, 250);
+
+        primaryStage.setTitle("Multiple Scenes Example");
+        primaryStage.setScene(scene1);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
