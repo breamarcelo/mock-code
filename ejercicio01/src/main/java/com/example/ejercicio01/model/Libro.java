756b2a7573e9a188da3bff18db004afaae834275
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-04-18 01:05:36 +0200

    updates

--- ejercicio01/src/main/java/com/example/ejercicio01/model/Libro.java
+++ ejercicio01/src/main/java/com/example/ejercicio01/model/Libro.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "TLibros")
 public class Libro {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
