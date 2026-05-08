de6a031e0e930d78d545c3a116272b77d1b92a2d
Marcelo Brea <43235037+breamarcelo@users.noreply.github.com>
2026-05-08 04:15:33 +0200

    basic UI created, needs http implementation

---
+++ mobile/src/app/home/home.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonButton],
+})
+export class HomePage {
+  constructor() { }
+}
